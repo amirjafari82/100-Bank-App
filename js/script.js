34aa05ff34709389e743e79b8275763e433ec68f
Amir <amiramjafaridev@gmail.com>
2024-08-02 17:01:33 +0330

    News Completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
     direction : 'horizontal',
     loop: 'true',
     slidesPerView: 4,
-    spaceBetween: 25,
+    spaceBetween: 20,
     navigation: {
         nextEl: '.button-next',
         prevEl: '.button-prev',
